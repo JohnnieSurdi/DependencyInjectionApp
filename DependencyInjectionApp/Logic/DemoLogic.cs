7f2ee3879567dcf36463bf9c5af3899561fd9e3d
JanSurdziel <jan.surdziel@easyjet.com>
2024-11-25 11:52:13 +0100

    Without DI

---
+++ DependencyInjectionApp/Logic/DemoLogic.cs
@@ -0,0 +1,13 @@
+﻿namespace DependencyInjectionApp.Logic;
+
+public class DemoLogic
+{
+    public int Value1 { get; private set; }
+    public int Value2 { get; private set; }
+
+    public DemoLogic()
+    {
+        Value1 = Random.Shared.Next(0, 1001);
+        Value2 = Random.Shared.Next(0, 1001);
+    }
+}
